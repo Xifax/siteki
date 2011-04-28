31b8da1cab7eaed3995da066f741270ba6bb3c27
Xifax <demi.log@gmail.com>
2011-04-29 01:53:38 +0400

    Updated install script

--- src/install.py
+++ src/install.py
@@ -13,33 +13,41 @@ def downloadWithProgressbar(url):
     urllib.urlretrieve(url, file_name, reporthook=dlProgress)
     return file_name
 
+def download_and_install(file_url):
+        file = downloadWithProgressbar(file_url)
+        subprocess.call('./' + file)
+        os.remove('./' + file)
 try:
     from setuptools.command import easy_install
 except ImportError:
     print 'Please, install easy_install!'
     if raw_input('Download setuptools now? [y/n]: ') == ('y' or 'Y'):
-        file = downloadWithProgressbar('http://pypi.python.org/packages/2.6/s/setuptools/setuptools-0.6c11.win32-py2.6.exe')
-        subprocess.call('./' + file)
-        os.remove('./' + file)
+        download_and_install('http://pypi.python.org/packages/2.6/s/setuptools/setuptools-0.6c11.win32-py2.6.exe')
     else: sys.exit(0)
 
 def install_with_easyinstall(package):
     try:
-        if package == 'pyqt': __import__('PyQt4')
-        else: __import__(package)
+        __import__(package)
         in_system.append(package)
     except ImportError:
         print 'Installing ' + package
-        easy_install.main(['-U', package])
-        installed.append(package)
+        try:
+            easy_install.main(['-U', package])
+            installed.append(package)
+        except Exception:
+            pass
 
 if __name__ == '__main__':
     installed = []; in_system = []
-    packages = ['userconfig', 'cjktools', 'cjktools-data', 'pyqt']
+    packages = ['userconfig', 'jcconv', 'cjktools', 'cjktools-data']
     for package in packages:
         install_with_easyinstall(package)
 
-    #TODO: for PyQt it may be better to use binary installer(s)
+    # PyQt
+    try:  __import__('PyQt4')
+    except ImportError: download_and_install('http://www.riverbankcomputing.co.uk/static/Downloads/PyQt4/PyQt-Py2.6-x86-gpl-4.8.3-1.exe')
+    # MeCab
+    download_and_install('http://sourceforge.net/projects/mecab/files/mecab-win32/0.98/mecab-0.98.exe/download')
 
     print 'Install/Update complete. Status:\n'
     print '\n'.join(installed), '\n\n(total installed: ' + str(len(installed)) + ')\n'
